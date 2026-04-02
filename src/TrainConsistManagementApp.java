20ab840df7532dbbfff36c5100423525d51012db
Evana-ad <evana.carrier@gmail.com>
2026-04-02 10:14:23 +0530

    UC9: Group Bogies by Type (Collectors.groupingBy)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,42 +1,48 @@
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.*;
 
 class Bogie {
-    String name;
+    String type;
     int capacity;
 
-    Bogie(String name, int capacity) {
-        this.name = name;
+    Bogie(String type, int capacity) {
+        this.type = type;
         this.capacity = capacity;
+    }
+
+    String getType() {
+        return type;
+    }
+
+    public String toString() {
+        return type + " Bogie | Capacity: " + capacity;
     }
 }
 
 public class TrainConsistManagementApp {
-
     public static void main(String[] args) {
-
-        System.out.println("=== Train Consist Management App ===");
-        System.out.println("\n--- Grouping Bogies by Type ---");
 
         List<Bogie> bogies = new ArrayList<>();
 
         bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("AC Chair", 60));
-        bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("First Class", 40));
-        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("AC Chair", 78));
+        bogies.add(new Bogie("Sleeper", 65));
+        bogies.add(new Bogie("First Class", 50));
+        bogies.add(new Bogie("AC Chair", 80));
+        bogies.add(new Bogie("First Class", 45));
 
         Map<String, List<Bogie>> groupedBogies =
                 bogies.stream()
-                        .collect(Collectors.groupingBy(b -> b.name));
+                        .collect(Collectors.groupingBy(Bogie::getType));
+
+        System.out.println("Bogies Grouped By Type:\n");
 
         for (String type : groupedBogies.keySet()) {
-            System.out.println(type + " Bogies:");
+            System.out.println("=== " + type + " ===");
             for (Bogie b : groupedBogies.get(type)) {
-                System.out.println("  Capacity: " + b.capacity);
+                System.out.println(b);
             }
+            System.out.println();
         }
-
-        System.out.println("\nProgram ready for next operations...");
     }
 }
