a9f32e3a91bfa0dcffde5aca4f8a9fa66655547b
Evana-ad <evana.carrier@gmail.com>
2026-04-02 09:52:48 +0530

    UC8: Filter Passenger Bogies Using Streams

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Bogie {
     String name;
@@ -17,19 +17,23 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
-        System.out.println("\n--- Sorting Bogies by Capacity ---");
+        System.out.println("\n--- Filtering High Capacity Bogies ---");
 
         List<Bogie> bogies = new ArrayList<>();
 
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 40));
+        bogies.add(new Bogie("Luxury Coach", 80));
 
-        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+        List<Bogie> highCapacityBogies =
+                bogies.stream()
+                        .filter(b -> b.capacity > 60)
+                        .collect(Collectors.toList());
 
-        System.out.println("Bogies sorted by capacity:");
+        System.out.println("Bogies with capacity > 60:");
 
-        for (Bogie b : bogies) {
+        for (Bogie b : highCapacityBogies) {
             System.out.println(b.name + " -> " + b.capacity + " seats");
         }
 
